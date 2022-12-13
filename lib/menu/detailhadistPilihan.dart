d6de79355bfe34252c07de6bf77afbe839efe54e
hanaghaliyahazhar <hanaghaliyahazhar@gmail.com>
2022-12-13 08:29:59 +0700

    fix null in detail hadist

--- lib/menu/detailhadistPilihan.dart
+++ lib/menu/detailhadistPilihan.dart
@@ -35,22 +35,29 @@ class _DetailHadistPilihanState extends State<DetailHadistPilihan> {
             Container(
                 margin: EdgeInsets.all(16),
                 padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(16)),
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(16)),
                 child: Column(
                   children: [
                     Text(
                       "${widget.model.arab.title}",
-                      style: TextStyle(color: Colors.white, fontSize: 16.0 * scale),
+                      style: TextStyle(
+                          color: Colors.white, fontSize: 16.0 * scale),
                       textAlign: TextAlign.center,
                     ),
                     Text(
                       "PELAJARAN TENTANG ${widget.model.indonesia.title}",
-                      style: TextStyle(color: Colors.white, fontSize: 16.0 * scale, fontWeight: FontWeight.bold),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0 * scale,
+                          fontWeight: FontWeight.bold),
                       textAlign: TextAlign.center,
                     ),
                     Text(
                       "${widget.model.jawa.title}",
-                      style: TextStyle(color: Colors.white, fontSize: 16.0 * scale),
+                      style: TextStyle(
+                          color: Colors.white, fontSize: 16.0 * scale),
                       textAlign: TextAlign.center,
                     )
                   ],
@@ -58,7 +65,8 @@ class _DetailHadistPilihanState extends State<DetailHadistPilihan> {
             Text(
               "Hadist",
               textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0 * scale),
+              style: TextStyle(
+                  fontWeight: FontWeight.bold, fontSize: 25.0 * scale),
             ),
             Container(
               height: 1.h,
@@ -72,20 +80,25 @@ class _DetailHadistPilihanState extends State<DetailHadistPilihan> {
                     itemCount: widget.model.arab.hadist.length,
                     itemBuilder: (context, index) {
                       return Container(
-                          decoration: BoxDecoration(color: Color.fromARGB(255, 234, 234, 235), borderRadius: BorderRadius.circular(16)),
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 234, 234, 235),
+                              borderRadius: BorderRadius.circular(16)),
                           padding: EdgeInsets.all(4),
-                          margin: EdgeInsets.only(right: 12, left: 12, bottom: 8),
+                          margin:
+                              EdgeInsets.only(right: 12, left: 12, bottom: 8),
                           child: Column(
                             children: [
                               ListTile(
                                 leading: CircleAvatar(
                                   backgroundColor: Colors.blue,
                                   child: Text(
-                                    widget.model.indonesia.hadist[index].id.toString(),
+                                    widget.model.indonesia.hadist[index].id
+                                        .toString(),
                                   ),
                                 ),
                                 title: Text(
-                                  widget.model.arab.hadist[index].value.toString(),
+                                  widget.model.arab.hadist[index].value
+                                      .toString(),
                                   textAlign: TextAlign.right,
                                   style: TextStyle(fontSize: 16 * scale),
                                 ),
@@ -93,7 +106,8 @@ class _DetailHadistPilihanState extends State<DetailHadistPilihan> {
                               Divider(),
                               ListTile(
                                 title: Text(
-                                  widget.model.indonesia.hadist[index].value.toString(),
+                                  widget.model.indonesia.hadist[index].value
+                                      .toString(),
                                   textAlign: TextAlign.justify,
                                   style: TextStyle(fontSize: 16 * scale),
                                 ),
@@ -101,7 +115,8 @@ class _DetailHadistPilihanState extends State<DetailHadistPilihan> {
                               Divider(),
                               ListTile(
                                 title: Text(
-                                  widget.model.jawa.hadist[index].value.toString(),
+                                  widget.model.jawa.hadist[index].value
+                                      .toString(),
                                   textAlign: TextAlign.justify,
                                   style: TextStyle(fontSize: 16 * scale),
                                 ),
@@ -112,44 +127,69 @@ class _DetailHadistPilihanState extends State<DetailHadistPilihan> {
             Container(
               height: 2.h,
             ),
-            Text(
-              "Firman",
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-            ),
-            Container(
-              height: 1.h,
-            ),
-            widget.model.arab.firman[0].value == null
-                ? Text("Firman belum tercantum")
-                : ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    physics: ScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: widget.model.arab.firman.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                          decoration: BoxDecoration(color: Color.fromARGB(255, 234, 234, 235), borderRadius: BorderRadius.circular(16)),
-                          padding: EdgeInsets.all(4),
-                          margin: EdgeInsets.only(right: 12, left: 12, bottom: 8),
-                          child: Column(
-                            children: [
-                              ListTile(
-                                leading: CircleAvatar(
-                                  backgroundColor: Colors.blue,
-                                  child: Text(widget.model.indonesia.firman[index].id.toString()),
-                                ),
-                                title: Text(widget.model.arab.firman[index].value.toString(), textAlign: TextAlign.right),
-                              ),
-                              ListTile(
-                                title: Text(widget.model.indonesia.firman[index].value.toString(), textAlign: TextAlign.justify),
-                              ),
-                              ListTile(
-                                title: Text(widget.model.jawa.firman[index].value.toString(), textAlign: TextAlign.justify),
-                              ),
-                            ],
-                          ));
-                    }),
+            widget.model.arab.firman[0].value == 'null'
+                ? Container(
+                    height: 1.h,
+                  )
+                : Column(
+                    children: [
+                      Text(
+                        "Firman",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 25),
+                      ),
+                      Container(
+                        height: 1.h,
+                      ),
+                      ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          physics: ScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: widget.model.arab.firman.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                                decoration: BoxDecoration(
+                                    color: Color.fromARGB(255, 234, 234, 235),
+                                    borderRadius: BorderRadius.circular(16)),
+                                padding: EdgeInsets.all(4),
+                                margin: EdgeInsets.only(
+                                    right: 12, left: 12, bottom: 8),
+                                child: Column(children: [
+                                  ListTile(
+                                    leading: CircleAvatar(
+                                      backgroundColor: Colors.blue,
+                                      child: Text(widget
+                                          .model.indonesia.firman[index].id
+                                          .toString()),
+                                    ),
+                                    title: Text(
+                                      widget.model.arab.firman[index].value
+                                          .toString(),
+                                      textAlign: TextAlign.right,
+                                      style: TextStyle(fontSize: 16 * scale),
+                                    ),
+                                  ),
+                                  ListTile(
+                                    title: Text(
+                                      widget.model.indonesia.firman[index].value
+                                          .toString(),
+                                      textAlign: TextAlign.justify,
+                                      style: TextStyle(fontSize: 16 * scale),
+                                    ),
+                                  ),
+                                  ListTile(
+                                    title: Text(
+                                      widget.model.jawa.firman[index].value
+                                          .toString(),
+                                      textAlign: TextAlign.justify,
+                                      style: TextStyle(fontSize: 16 * scale),
+                                    ),
+                                  ),
+                                ]));
+                          })
+                    ],
+                  )
           ],
         )));
   }
